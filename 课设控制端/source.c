b5ac1bb4ee4cfb9b0c123042dd1cb20e9562a7c5
ZoraGK <jyh199759@126.com>
2018-12-10 23:29:17 +0800

    Update source.c

--- 课设控制端/source.c
+++ 课设控制端/source.c
@@ -7,60 +7,47 @@ int g_fdLight;	//灯光
 int g_fdCurtain;//窗帘
 int g_fdAirConditioner;//空调
 
-int InitSerial(int fd, int baudrate)//初始化串口
-{ 
-	return fd;
-}
+int main()
+{}
 
-void LightingCtrl (char portID,char state)//灯光控制系统，实现获取光照强度并改变灯光亮灭状态
-{
-	InitSerial(g_fdLight,9600);
-	char cmd[8];
-	SendCmd(cmd,portID);//向传感器发送命令读取光照强度
-	cmd=ReceiveCmd();//传感器接收光照强度的数据
-	illuminationLevel=DataProcessing(cmd);//获取处理后的光强数据
-}
+struct listener(var socketData)//循环执行，监听端口指令，返回指令数据的清洗后内容
+{}
 
-void CurtainCtrl (char portID,char state)//窗帘控制系统，实现点击按钮改变窗帘开合状态
-{
-	InitSerial(g_fdCurtain,9600);
-	char cmd[8];
-	SendCmd(cmd,portID);//根据窗帘的开合状态向传感器发送命令改变状态
-}
+int initSerial(int fd, int baudrate)//初始化串口
+{}
 
-void AirConditionerCtrl (char portID,char state)//空调控制系统，实现获取温度强度并改变空调温度
-{
-	InitSerial(g_fdAirConditioner,9600);
-	char cmd[8];
-	SendCmd(cmd,portID);//向传感器发送命令读取温度强度
-	cmd=ReceiveCmd();//传感器接收温度强度的数据
-	temperatureLevel=DataProcessing(cmd);//获取处理后的温度数据
-}
+var socket()//数据传递部分，等待下版本处理
+{}
 
-void SendCmd(char cmd,int portID)//向传感器发送指令。cmd：指令；portID：端口号；flag：值为1表示发送成功
-{
-	short flag=0;
-	while(flag==0)
-	{
-		
-	}
-}
+var processer()//根据指令信息调用相应函数
+{}
 
-int ReceiveCmd()//从传感器接收指令
-{
-	char cmd[8];	//cmd：接收到的指令；
-	short flag=0;
-	while(flag==0)
-	{
-		
-	}
-	return cmd;
-}
+int getLightLevel(char portID)//通过参数提供的串口进行数据获取，返回光照强度十进制数据。
+{}
 
-int DataProcessing(char cmd)//对接收的数据进行处理得到光强
-{
-	return dataLevel;
-}
+int lightingCtrl (char portID)//通过参数提供的串口对灯光进行控制，将灯光取反，返回当前灯光状态，亮1灭0。
+{}
+
+int getCurtain(char portID)//通过参数提供的串口返回窗帘当前状态值
+{}
+
+void curtainCtrl (char portID,char state)//通过参数提供的串口和状态值对窗帘进行控制，将窗帘状态设为参数状态值
+{}
+
+void airConditionerCtrl (char portID,char state)//通过参数提供的串口和状态值改变空调温度
+{}
+
+int getAirConditioner(char portID)//通过参数提供的串口获取空调当前温度
+{}
+
+int sendCmd(char cmd,int portID)//向传感器发送指令。cmd：指令；portID：端口号；flag：值为1表示发送成功
+{}
+
+int receiveCmd()//从传感器接收指令
+{}
+
+int dataProcessing(char cmd)//对接收的数据进行处理得到光强
+{}
 
 
 
